f03da1492bea9501eac0e3a597b4f404b0629f23
Aschalew Daraje <aschalewdere2@gmail.com>
2025-04-04 20:19:58 -0700

    A minimize code force problem solved

---
+++ python implemantation/a mininmize.py
@@ -0,0 +1,19 @@
+t=int(input())
+ans=[]
+for _ in range(t):
+    current_min= float('inf')
+    a,b=map(int,input().split())
+    for c in range(a,b+1):
+        mini=(c-a)+(b-c)
+        if a==b:
+            current_min=0
+            break
+        else:
+            if mini<current_min:
+                current_min=mini
+    ans.append(current_min)
+for i in ans:
+    print(i)
+
+
+
