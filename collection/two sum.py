9114b7abac7eba125922c326fde5c3181b0429af
Aschalew Daraje <aschalewdere2@gmail.com>
2025-05-07 08:23:38 -0700

    two sum leet code problem solved

---
+++ collection/two sum.py
@@ -0,0 +1,18 @@
+nums = [3, 3]
+target = 6
+
+# Create a list of tuples (value, original index) and sort by value
+temp = sorted([(num, i) for i, num in enumerate(nums)], key=lambda x: x[0])
+
+l, r = 0, len(temp) - 1
+
+while l < r:
+    current_sum = temp[l][0] + temp[r][0]
+    if current_sum == target:
+        # Return the original indices from the tuples
+        print([temp[l][1], temp[r][1]])
+        break
+    elif current_sum > target:
+        r -= 1
+    else:
+        l += 1
