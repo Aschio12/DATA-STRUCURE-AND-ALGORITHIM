6b7f56e6ae0e7250eab40e6c54bfcee47e5e769d
Aschalew Daraje <aschalewdere2@gmail.com>
2025-06-11 12:11:54 -0700

    winners 1500 level problem solved

---
+++ codeforce/1500/A_Winner.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+def main():
+    n = int(input().strip())
+    scores = defaultdict(int)
+    history = []
+    
+    for _ in range(n):
+        data = input().split()
+        name = data[0]
+        score = int(data[1])
+        scores[name] += score
+        history.append((name, scores[name]))
+    
+    max_score = max(scores.values())
+    candidates = {player for player, total in scores.items() if total == max_score}
+    
+    for name, total in history:
+        if name in candidates and total == max_score:
+            return(name)
+print(main())
